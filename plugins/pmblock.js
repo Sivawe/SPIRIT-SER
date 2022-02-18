14c2ad850eae6528a0cee38327f6b8ad058c7892
SPIRIT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2022-02-18 20:42:42 +0530

    Update pmblock.js

--- plugins/pmblock.js
+++ plugins/pmblock.js
@@ -7,7 +7,7 @@ const Config = require('../config')
 
 // Inbox Block System
 // This Plugins By Hisham Muhammed 
-const INBO = "INBO BLOCK NOT ALLOWED 🖕"
+const INBO = "INBOX NOT ALLOWED ᴄʟɪᴄᴋ ᴛʜɪs ʟɪɴᴋ ᴊᴏɪɴ sᴘɪʀɪᴛ sᴇᴅ ᴡʜᴀᴛsᴀᴘᴘ ɢʀᴜᴏᴘ https://chat.whatsapp.com/F3adhxvYpd3KzVptzDxhC4"
  if (Config.INBO == 'true') {
 Hisham.addCommand({on: 'text', fromMe: false, delownsewcmd: false, onlyPm: true }, (async (message, match) => {
         let regexb1ichu = new RegExp('.')
