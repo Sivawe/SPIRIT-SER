7f47f2b08608099b4ab2a63403e2870f3bcf1fe2
SPIRIT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2022-03-06 12:08:28 +0530

    Update spiritbotv2.js

--- plugins/spiritbotv2.js
+++ plugins/spiritbotv2.js
@@ -7,21 +7,21 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/Mbaz2g9.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/iQArLPp.jpeg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `Bᴏᴛ Nᴀᴍᴇ  Sᴘɪʀɪᴛ sᴇʀ Bᴏᴛ 
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `Bᴏᴛ Nᴀᴍᴇ  Sᴘɪʀɪᴛ sᴇʀ Bᴏᴛ💞 
 
 ᴄʀᴇᴀᴛᴏʀ ɴᴜᴍʙᴇʀ: wa.me/917994093863
 
 sᴘɪʀɪᴛ ʙᴏᴛ ɢʀᴏᴜᴘ: https://chat.whatsapp.com/F3adhxvYpd3KzVptzDxhC4
 
-Gɪᴛʜᴜʙ ʟɪɴᴋ:  https://github.com/SPlRlT-YT/SPIRIT-SER
+Gɪᴛʜᴜʙ ʟɪɴᴋ: https://github.com/SPlRlT-YT/SPIRIT-SER
 
-Aᴜᴅɪᴏ ᴄᴏᴍᴍᴀᴅᴇ :   https://github.com/SPlRlT-YT/SPIRIT-SER/tree/master/uploads
+Aᴜᴅɪᴏ ᴄᴏᴍᴍᴀᴅᴇ :  https://github.com/SPlRlT-YT/SPIRIT-SER/tree/master/uploads
 
 Sᴛɪᴄᴋᴇʀ ᴄᴏᴍᴍᴀᴅᴇ: https://github.com/SPlRlT-YT/SPIRIT-SER/tree/master/Spiritser
 
-Cʜᴀɴɴᴇʟ :  https://youtube.com/channel/UCBv1Z1zkOmdGeWU2QRdYBnA
+Cʜᴀɴɴᴇʟ : https://youtube.com/channel/UCBv1Z1zkOmdGeWU2QRdYBnA
 
 💝sᴘɪʀɪᴛ Sᴇʀ Bᴏᴛ Cʀᴇᴀᴛᴇᴅ Bʏ Sᴘɪʀɪᴛ Sᴇʀ 💝
 `}) 
